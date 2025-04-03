6af86c912ed3668c1a735ef16a96bbade029793a
VersLeSoleil <425094746@qq.com>
2025-04-03 16:34:14 +0800

    解决冲突

--- extension.js
+++ extension.js
@@ -1,4 +1,5 @@
-
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const { activateCodeCompletion } = require('./src/codeCompletion');
 const { activateAiChatCodeGen } = require('./src/aiChatCodeGen');
